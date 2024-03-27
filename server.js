750fd7bcbc24620cee068fecc5aeaac3b075080a
Tim Wallace <timwallacedev@gmail.com>
2024-03-27 19:29:35 -0400

    add endpoint for getting inventory from single warehouse

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const {inventoryRouter} = require("./routes/inventory.js")
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
+app.use("/api", inventoryRouter)
 
 app.get("/", (_req, res) => {
     res.send("Hello");
