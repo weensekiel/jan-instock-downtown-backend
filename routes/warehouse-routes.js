d56f4b17d026f9658df4bdb023c1eb67dad43c91
Ezekiel <evangelista.ezekiel@hotmail.ca>
2024-03-28 15:26:36 -0400

    add new warehouse route and controller

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   all,
   findOne,
+  addWarehouse,
   deleteOne,
 } from "../controllers/warehouse-controller.js";
 
 const warehousesRouter = express.Router();
 
 warehousesRouter.route("/").get(all);
+warehousesRouter.route("/").post(addWarehouse);
 
 warehousesRouter.route("/:id").delete(deleteOne);
 
