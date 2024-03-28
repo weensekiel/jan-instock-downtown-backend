b74e0ecea4790683448e05b88ea6b4a347494cc5
alueddeke <a.lueddeke@hotmail.com>
2024-03-28 12:53:27 -0400

    updated to es6 syntax, added controller files, updated knexfile syntax

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import warehousesRouter from "./routes/warehouses-routes.js";
+import warehousesRouter from "./routes/warehouse-routes.js";
 
-const express = require("express");
-const cors = require("cors");
-const {inventoryRouter} = require("./routes/inventory.js")
-
-dotenv.config();
-const app = express();
-
-app.use(warehousesRouter);
-app.use(cors());
-// is there a way to update this to change, that PORT would be in the front-end ENV and not available here, no?
+import { inventoryRouter } from "./routes/inventory-routes.js";
 
 const PORT = process.env.PORT;
-const SingleWarehouse = require("./routes/SingleWarehouse");
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/warehouses/:warehouseId", SingleWarehouse);
+app.use("/api/warehouses", warehousesRouter);
 
-app.use(cors())
-app.use(express.json())
-app.use("/api", inventoryRouter)
+app.use("/api", inventoryRouter);
 
 app.get("/", (_req, res) => {
   res.send("Hello");
