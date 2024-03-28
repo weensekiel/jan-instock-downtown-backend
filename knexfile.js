128388785c1038397da952810c67df84cebb55b6
Nicholas Cotton <nic@nintu.net>
2024-03-27 21:52:52 -0400

    Catchup to develop branch

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,12 @@
 // Update with your config settings.
-require("dotenv").config()
+require("dotenv").config();
 module.exports = {
-
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      charset: "utf-8",
-    }
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    charset: "utf8",
   },
 };
--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
+// is there a way to update this to change, that PORT would be in the front-end ENV and not available here, no?
 
 const PORT = process.env.PORT;
+const SingleWarehouse = require("./routes/SingleWarehouse");
+
+app.use(express.json());
+
+app.get("/warehouses/:warehouseId", SingleWarehouse);
 
 app.get("/", (_req, res) => {
-    res.send("Hello");
-})
-
+  res.send("Hello");
+});
 
 app.listen(PORT, () => {
-    console.log(`running at http://localhost:${PORT}`);
+  console.log(`running at http://localhost:${PORT}`);
 });
