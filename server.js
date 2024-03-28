4ea74f2d327adf2277a5bba3362bf18cb5ce8e41
alueddeke <a.lueddeke@hotmail.com>
2024-03-27 20:53:30 -0400

    started adding route structures for get all warehouses endpoint, need to connect to database

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import warehousesRouter from "./routes/warehouses-routes.js";
 
-const express = require("express");
-const cors = require("cors");
+//can't find this file
 
+dotenv.config();
 const app = express();
+
+app.use(warehousesRouter);
 
 const PORT = process.env.PORT;
 
 app.get("/", (_req, res) => {
-    res.send("Hello");
-})
-
+  res.send("Hello");
+});
 
 app.listen(PORT, () => {
-    console.log(`running at http://localhost:${PORT}`);
+  console.log(`running at http://localhost:${PORT}`);
 });
